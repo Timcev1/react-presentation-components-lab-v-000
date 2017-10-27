d94a743f0489c38900dee963dc939b29c1689086
Timothy Cevallos <timcev@gmail.com>
2017-10-27 16:36:59 +0000

    automatically backed up by learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,6 +1,6 @@
 // Code SimplerComponentHere Here
 import React from 'react';
 
-const SimplerComponent = props => <div onClick={props.handleClick}>clickie clicke.</div>;
+const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy.</div>;
 
 export default SimplerComponent;
